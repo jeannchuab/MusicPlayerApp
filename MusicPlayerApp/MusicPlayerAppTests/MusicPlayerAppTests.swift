a8e8e11ac54046ddcd83b4b19134ca9e868190bd
jeannchuab <jeann.chuab@gmail.com>
2026-04-19 11:42:25 -0300

    Initial project setup

---
+++ MusicPlayerApp/MusicPlayerAppTests/MusicPlayerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerAppTests.swift
+//  MusicPlayerAppTests
+//
+//  Created by Jeann Luiz Chuab on 19/04/26.
+//
+
+import Testing
+@testable import MusicPlayerApp
+
+struct MusicPlayerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
